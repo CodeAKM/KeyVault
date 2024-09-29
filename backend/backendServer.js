e43211d4627fc2dc22d8d5e7ae0f813cfaaac4b9
Aman Kumar <trulyaman25@gmail.com>
2024-09-29 15:07:02 +0530

    Updated MongoDB URI

--- backend/backendServer.js
+++ backend/backendServer.js
@@ -7,12 +7,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect("mongodb+srv://amanwhoooo:VuKVdMuGEHjQRmOG@cluster0.vygshnz.mongodb.net/keyVault")
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.error("Error connecting to MongoDB:", err));
 
